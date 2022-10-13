2f2e6ac74e2d146274a25bb71050fcb2ca3b77f7
Jorge Soto Garcia <sotogarcia@gmail.com>
2022-10-13 11:54:11 +0200

    [FIX] some stuff was removed

--- __discard__/academy_tests_web/static/src/js/academy_post_answers_table_template.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-
-    $('head').append('<link rel="stylesheet" type="text/css" href="/academy_tests_web/static/src/css/academy_post_answers_table_template.css">');
-});
